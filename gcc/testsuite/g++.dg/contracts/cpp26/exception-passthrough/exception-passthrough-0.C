57afe128e215ce585c71ff780b15c5d79d88880f
Ville Voutilainen <ville.voutilainen@gmail.com>
2025-09-05 20:55:21 +0300

    c++, contracts: Villes disable exception translation.
    Add a flag that disables passing predicate exceptions to the violation handler
    
    Fix the diagnostic for -fcontracts-disable-predicate-exception-translation

---
+++ gcc/testsuite/g++.dg/contracts/cpp26/exception-passthrough/exception-passthrough-0.C
@@ -0,0 +1,23 @@
+// { dg-do run { target c++20 } }
+// { dg-additional-options "-fcontracts -fcontract-evaluation-semantic=observe -fcontracts-disable-predicate-exception-translation" }
+
+#include <cassert>
+
+bool check(int i){
+  if (i > 10)
+    throw 3;
+
+  return true;
+}
+
+void f() pre(check(15)){}
+
+
+int main(int, char**)
+{
+  try {
+    f();
+  } catch (const int& x) {
+    assert(x == 3);
+  }
+}
