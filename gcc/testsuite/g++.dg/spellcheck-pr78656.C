785648a19761ec2ef02ded479c077c2cd9bc8f79
Jakub Jelinek <jakub@redhat.com>
2025-09-15 09:35:13 +0200

    libstdc++, v3: library side of C++26 P2786R13 - Trivial Relocatability [PR119064]
    Hi!
    
    Here is a new version of the library side of trivial relocatability,
    changed to use std::start_lifetime_as if it is supported (so on top of
    https://gcc.gnu.org/pipermail/gcc-patches/2025-August/693683.html
    and
    https://gcc.gnu.org/pipermail/gcc-patches/2025-August/693665.html
    ).
    
    On Tue, Aug 26, 2025 at 03:28:43PM +0200, Tomasz Kaminski wrote:
    > I would prefer this function to be templated on the tested type (S or T),
    > instead of having the code duplicated. This should reduce the test  code by
    > half.
    
    Good idea, done.
    
    > Instead of destroying and then re-constructing local objects, you could now
    > use trivial_union
    > that I have sent in another email, to store the arrays.
    
    Is something wrong on the way the test tests it, or is testing through
    trivial union just another way that it should be tested on?
    
    Bootstrapped/regtested on x86_64-linux and i686-linux.
    
    2025-09-15  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/119064
    	* include/bits/version.def (trivially_relocatable): New.
    	* include/bits/version.h: Regenerate.
    	* include/std/type_traits (std::is_trivially_relocatable,
    	std::is_nothrow_relocatable, std::is_replaceable): New traits.
    	std::is_trivially_relocatable_v, std::is_nothrow_relocatable_v,
    	std::is_replaceable_v): New trait variable templates.
    	* include/std/memory (__glibcxx_want_trivially_relocatable): Define
    	before including bits/version.h.
    	(std::trivially_relocate): New template function.
    	(std::relocate): Likewise.
    	* testsuite/std/memory/relocate/relocate.cc: New test.
    
    	* g++.dg/spellcheck-pr78656.C: For c++26 expect relocate rather than
    	allocator as spelling hint.

--- gcc/testsuite/g++.dg/spellcheck-pr78656.C
+++ gcc/testsuite/g++.dg/spellcheck-pr78656.C
@@ -4,15 +4,21 @@
 
 void* allocate(std::size_t n)
 {
-  return std::allocate<char>().allocate(n); // { dg-error ".allocate. is not a member of .std.; did you mean 'allocator'\\?" }
+  // { dg-error ".allocate. is not a member of .std.; did you mean 'relocate'\\?" "" { target c++26 } .+1 }
+  return std::allocate<char>().allocate(n); // { dg-error ".allocate. is not a member of .std.; did you mean 'allocator'\\?" "" { target c++23_down } }
   /* { dg-begin-multiline-output "" }
    return std::allocate<char>().allocate(n);
                ^~~~~~~~
                allocator
-     { dg-end-multiline-output "" } */
+     { dg-end-multiline-output "" { target c++23_down } } */
+  /* { dg-begin-multiline-output "" }
+   return std::allocate<char>().allocate(n);
+               ^~~~~~~~
+               relocate
+     { dg-end-multiline-output "" { target c++26 } } */
 
   // Various errors follow that we don't care about; suppress them:
-  // { dg-excess-errors "7: " }
+  // { dg-excess-errors "8: " }
 }
 
 void* test_2(std::size_t n)
@@ -25,5 +31,5 @@ void* test_2(std::size_t n)
      { dg-end-multiline-output "" } */
 
   // Various errors follow that we don't care about; suppress them:
-  // { dg-excess-errors "25: " }
+  // { dg-excess-errors "26: " }
 }
