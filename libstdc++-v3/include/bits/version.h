7cc5d11de9a5717fdba10b2cc16f0b21f4705f5e
Jakub Jelinek <jakub@redhat.com>
2025-09-15 09:35:13 +0200

    libstdc++, v3: library side of C++26 P2786R13 - Trivial Relocatability [PR119064]
    Hi!
    
    Here is a new version of the library side of trivial relocatability,
    changed to use std::start_lifetime_as if it is supported (so on top of
    https://gcc.gnu.org/pipermail/gcc-patches/2025-August/693683.html
    and
    https://gcc.gnu.org/pipermail/gcc-patches/2025-August/693665.html
    ).
    
    On Tue, Aug 26, 2025 at 03:28:43PM +0200, Tomasz Kaminski wrote:
    > I would prefer this function to be templated on the tested type (S or T),
    > instead of having the code duplicated. This should reduce the test  code by
    > half.
    
    Good idea, done.
    
    > Instead of destroying and then re-constructing local objects, you could now
    > use trivial_union
    > that I have sent in another email, to store the arrays.
    
    Is something wrong on the way the test tests it, or is testing through
    trivial union just another way that it should be tested on?
    
    Bootstrapped/regtested on x86_64-linux and i686-linux.
    
    2025-09-15  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/119064
    	* include/bits/version.def (trivially_relocatable): New.
    	* include/bits/version.h: Regenerate.
    	* include/std/type_traits (std::is_trivially_relocatable,
    	std::is_nothrow_relocatable, std::is_replaceable): New traits.
    	std::is_trivially_relocatable_v, std::is_nothrow_relocatable_v,
    	std::is_replaceable_v): New trait variable templates.
    	* include/std/memory (__glibcxx_want_trivially_relocatable): Define
    	before including bits/version.h.
    	(std::trivially_relocate): New template function.
    	(std::relocate): Likewise.
    	* testsuite/std/memory/relocate/relocate.cc: New test.
    
    	* g++.dg/spellcheck-pr78656.C: For c++26 expect relocate rather than
    	allocator as spelling hint.

--- libstdc++-v3/include/bits/version.h
+++ libstdc++-v3/include/bits/version.h
@@ -2504,4 +2504,14 @@
 #endif /* !defined(__cpp_lib_is_implicit_lifetime) */
 #undef __glibcxx_want_is_implicit_lifetime
 
+#if !defined(__cpp_lib_trivially_relocatable)
+# if (__cplusplus >  202302L) && (__cpp_trivial_relocatability >= 202502L)
+#  define __glibcxx_trivially_relocatable 202502L
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_trivially_relocatable)
+#   define __cpp_lib_trivially_relocatable 202502L
+#  endif
+# endif
+#endif /* !defined(__cpp_lib_trivially_relocatable) */
+#undef __glibcxx_want_trivially_relocatable
+
 #undef __glibcxx_want_all
